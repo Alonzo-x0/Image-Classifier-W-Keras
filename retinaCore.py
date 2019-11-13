b22f09aa70d0521722c1c624882a2716afa2b4a6
Alonzo <arizagapepe@gmail.com>
2019-11-13 12:57:47 -0500

    Added if and try statement to check path
    Checks to see if path doesn't exists, if it doesn't it will create the path, which is later used in a flow() walk to save manipulated images to path

--- retinaCore.py
+++ retinaCore.py
@@ -1,4 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import os
+
+
+os.environ['KMP_WARNINGS'] = '0'
+#disable above if you need to see tensorflow start logs
 
 datagen = ImageDataGenerator(
 	rotation_range=180, 
@@ -12,15 +17,27 @@ datagen = ImageDataGenerator(
 	#validation_split=0.2, 
 	fill_mode='nearest')
 
-img = load_image('dataset/dataset/test_set/cats/cat.4001')
+img = load_img('dataset/dataset/test_set/cats/cat.4001.jpg')
 
 imgArray = img_to_array(img)
+
+
+path = 'imgPreview'
+if os.path.exists(path) == False:
+	print(f'{path} does not exist')
+	try:
+		os.mkdir(path)
+		print(f'{path} has been made')
+	except OSError:
+		print(f'Directory {path} could not be made')
+else:
+	print(f'{path} already exists')
 
 #arImage = array_to_img(imgArray)
 
 imgArray = imgArray.reshape((1,) + imgArray.shape)
 i = 0
-for batch in datagen.flow(imgArray, batch_size=32, save_to_dir='imgPreview', save_prefix='cats', save_format='jpeg'):
+for batch in datagen.flow(imgArray, batch_size=32, save_to_dir=path, save_prefix='cats', save_format='jpeg'):
 	i += 1
 
 	if i > 20:
