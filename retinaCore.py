c881d73ae516a2c87518e0653dd2af981c21002b
Alonzo <arizagapepe@gmail.com>
2019-11-27 13:48:02 -0500

    Completed project using elements from my previous ML
    Uses a much less code and switched to GPU tensorflow to make it quicker, around .79-.81 ACC

--- retinaCore.py
+++ retinaCore.py
@@ -1,9 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import os
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import SeparableConv2D, BatchNormalization, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.layers.convolutional import SeparableConv2D
+from keras import backend as K
 
 
 os.environ['KMP_WARNINGS'] = '0'
@@ -21,10 +21,11 @@ datagen = ImageDataGenerator(
 	#validation_split=0.2, 
 	fill_mode='nearest')
 
-img = load_img('dataset/dataset/test_set/cats/cat.4001.jpg')
+img = load_img('data/train/cats/cat.1.jpg')
 
 imgArray = img_to_array(img)
-
+imgArray = imgArray.reshape((1,) + imgArray.shape)
+#print(imgArray.shape)
 
 path = 'imgPreview'
 if os.path.exists(path) == False:
@@ -39,40 +40,82 @@ else:
 
 #arImage = array_to_img(imgArray)
 
-imgArray = imgArray.reshape((1,) + imgArray.shape)
 i = 0
-for batch in datagen.flow(imgArray, batch_size=32, save_to_dir=path, save_prefix='cats', save_format='jpeg'):
+for batch in datagen.flow(imgArray, batch_size=1, save_to_dir=path, save_prefix='cats', save_format='jpeg'):
 	i += 1
 
 	if i > 20:
 		break
 
 model = Sequential()
+
+shape=(150, 150, 3)
+channelDim = -1
+
+if K.image_data_format() == 'channels_first':
+	shape = (depth, height, width)
+	channelDim = 1
+
 model.add(SeparableConv2D(32, (3, 3), padding = 'same', input_shape = shape))
-model.add(Activation('relu'))		
+model.add(Activation('relu'))
+model.add(BatchNormalization(axis = channelDim))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
+
+model.add(SeparableConv2D(64, (3, 3), padding = 'same'))
+model.add(Activation('relu'))
+model.add(BatchNormalization(axis = channelDim))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(SeparableConv2D(64, (3, 3), padding = 'same'))
-model.add(Activation('relu'))		
-model.add(SeparableConv2D(64, (3, 3), padding = 'same'))
-model.add(Activation('relu'))		
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.25))
+
 model.add(SeparableConv2D(128, (3, 3), padding = 'same'))
 model.add(Activation('relu'))
-model.add(SeparableConv2D(128, (3, 3), padding = 'same'))
-model.add(Activation('relu'))
-model.add(SeparableConv2D(128, (3, 3), padding = 'same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.25))
+model.add(BatchNormalization(axis = channelDim))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(256))
-model.add(Activation('relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Activation('softmax'))
+model.add(Dense(64))
 
-opt=Adagrad(lr=INIT_LR, decay=INIT_LR/NUM_EPOCHS)
-model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(Dense(1))
+
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+
+batch_size = 16
+
+train_Datagen = ImageDataGenerator(
+	rescale=1/255,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True)
+
+test_Datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_Datagen.flow_from_directory(
+	'data/train',
+	target_size=(150, 150),
+	batch_size=batch_size,
+	class_mode='binary')
+
+validation_generator = test_Datagen.flow_from_directory(
+	'data/validation',
+	target_size=(150, 150),
+	batch_size=batch_size,
+	class_mode='binary')
+
+model.fit_generator(
+	train_generator,
+	steps_per_epoch=2000//batch_size,
+	epochs=50,
+	validation_data=validation_generator,
+	validation_steps=800//batch_size)
+
+model.save_weights('first_try.h5')
+
+
+
+
+
+
