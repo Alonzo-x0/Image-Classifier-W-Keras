d2828f1149811f1dbeb4b08366290afafc682999
Alonzo <arizagapepe@gmail.com>
2019-11-29 16:26:23 -0500

    Added load weights option and fixed load_img directory

--- retinaCore.py
+++ retinaCore.py
@@ -1,5 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import os
+from keras.models import Sequential
+from keras.layers import SeparableConv2D, BatchNormalization, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
 
 
 os.environ['KMP_WARNINGS'] = '0'
@@ -17,7 +21,7 @@ datagen = ImageDataGenerator(
 	#validation_split=0.2, 
 	fill_mode='nearest')
 
-img = load_img('dataset/dataset/test_set/cats/cat.4001.jpg')
+img = load_img('data/test/cats/cat.4001.jpg')
 
 imgArray = img_to_array(img)
 
@@ -42,5 +46,76 @@ for batch in datagen.flow(imgArray, batch_size=32, save_to_dir=path, save_prefix
 
 	if i > 20:
 		break
+
+model = Sequential()
+
+shape=(150, 150, 3)
+channelDim = -1
+
+if K.image_data_format() == 'channels_first':
+	shape = (depth, height, width)
+	channelDim = 1
+
+model.add(SeparableConv2D(32, (3, 3), padding = 'same', input_shape = shape))
+model.add(Activation('relu'))
+model.add(BatchNormalization(axis = channelDim))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
+
+model.add(SeparableConv2D(64, (3, 3), padding = 'same'))
+model.add(Activation('relu'))
+model.add(BatchNormalization(axis = channelDim))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(SeparableConv2D(128, (3, 3), padding = 'same'))
+model.add(Activation('relu'))
+model.add(BatchNormalization(axis = channelDim))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(Dense(1))
+
+model.add(Activation('sigmoid')) 
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+
+batch_size = 16
+
+train_Datagen = ImageDataGenerator(
+	rescale=1/255,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True)
+
+test_Datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_Datagen.flow_from_directory(
+	'data/train',
+	target_size=(150, 150),
+	batch_size=batch_size,
+	class_mode='binary')
+
+validation_generator = test_Datagen.flow_from_directory(
+	'data/validation',
+	target_size=(150, 150),
+	batch_size=batch_size,
+	class_mode='binary')
+model.load_weights('first_try.h5')
+model.fit_generator(
+	train_generator,
+	steps_per_epoch=2000//batch_size,
+	epochs=50,
+	validation_data=validation_generator,
+	validation_steps=800//batch_size)
+
+model.save_weights('first_try.h5')
+
+
+
+
 
 
